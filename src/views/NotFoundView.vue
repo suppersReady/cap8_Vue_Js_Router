3ba1be58f76c0fe2665f87a92f60ba55e22554a3
suppersReady <65486151+suppersReady@users.noreply.github.com>
2024-10-02 00:00:36 +0200

    first branch

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,11 +1,8 @@
 <template>
-    <div class="flex flex-col max-w-5xl p-8 mx-auto">
-        <h1 class="text-2xl font-bold mx-auto">404:Pagina non trovata</h1>
-        <img src="images/not_found.png" class="mx-auto" width="250" />
-    </div>
+  <div class="flex flex-col max-w-5xl p-8 mx-auto">
+    <h1 class="text-2xl font-bold mx-auto">404:Pagina non trovata</h1>
+  </div>
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
